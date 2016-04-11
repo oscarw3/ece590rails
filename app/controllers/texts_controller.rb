44c9b0d7954e497552e8d4672dbc9e2b0080f8f8
oscarw3 <oscarw3@gmail.com>
2016-04-11 18:04:26 -0400

    first commit of twilio for ece 590

---
+++ app/controllers/texts_controller.rb
@@ -0,0 +1,28 @@
+class TextsController < ApplicationController
+  respond_to :json
+  skip_before_filter :verify_authenticity_token
+
+  def index
+  	render json: { error: 'Access Denied'}, status: 401
+  end
+
+  def create
+  	twilio_sid = "ACd4c7174bd37083fd95f8661c0c3d89a0"
+	twilio_token = "5bca1b3ae1171cad2445f85052a9af22"
+	@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+	
+	tonumber = params["text"]["tonumber"]
+	fromnumber = params["text"]["fromnumber"]
+	message = params["text"]["message"]
+	 @twilio_client.account.messages.create(
+      :from => fromnumber,
+      :to => tonumber,
+      :body => message
+    )
+
+	 render json: "Message sent"
+  end
+
+
+end
